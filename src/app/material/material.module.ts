ee5260414e469625e1caaada0d7f11ad881600af
Sean Lawrenz <seanlaw27@gmail.com>
2021-02-03 15:43:37 -0500

    update material to 8

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatRadioButton, MatInputModule, MatFormField } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioButton } from '@angular/material/radio';
 
 const materialModules = [
   MatButtonModule,
