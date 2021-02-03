aa433c651d42684fe931ecd8083a70fc1c9decc2
Sean Lawrenz <seanlaw27@gmail.com>
2021-02-03 16:23:55 -0500

    update material module and theme

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatRadioButton } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
 
 const materialModules = [
   MatButtonModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule
 ];
 @NgModule({
   imports: materialModules,
